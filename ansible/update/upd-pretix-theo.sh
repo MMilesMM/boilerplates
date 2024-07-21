d9468db1deb7ac00bd7c33e6e80a8bc54f0c6064
Miles <miles@mrqdt.org>
2024-07-21 03:51:43 +0200

    Update upd-pretix-theo.sh

--- ansible/update/upd-pretix-theo.sh
+++ ansible/update/upd-pretix-theo.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+echo "Updating Pretix..."
+
+docker pull pretix/standalone:stable #Downloading new image
+
+docker build . -t mypretix #Building custom Dockerfile
+
+systemctl restart pretix.service #Restarting pretix service to load new Dockerimage
+
+sleep  15
+echo "Waiting 15 seconds for the docker container"
+
+docker exec -it pretix.service pretix upgrade #Run Post-Upgrade Skript
